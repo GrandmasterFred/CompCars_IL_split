3869c8291969fa7b98470d3d676c2c4ad65c879c
GMFred <95758535+GrandmasterFred@users.noreply.github.com>
2025-08-22 16:19:29 +0800

    updated data

--- CompCars_IL_split.py
+++ CompCars_IL_split.py
@@ -2,4 +2,8 @@
 this file splits compcars into tasks based on the year each of the models has been released. 
 
 This draws on the .json file, and each vehicle's date of release/production
+
+and with those production dates, it will sort out how many there are, and then also make it so that 
+
+also, i should re-tag them with the proper info that i forgot to carry over from the original data 
 """
---
+++ compcars_fill_in_gaps.py
@@ -0,0 +1,39 @@
+"""
+this file will be used to fille in the gaps for the vehicles that do not have a searchable production/one the road day
+"""
+
+from sp_functions import load_dict_from_json, save_dict_to_json
+
+import re
+
+def contains_valid_year(s):
+    match = re.search(r"\b(19[0-9]{2}|20[0-9]{2}|2100)\b", s)
+    return match.group(0) if match else None
+
+# loading the modified wiki-scraped data
+
+TARGET_SAVE = r'car_release_dates_wiki3_MANUAL_EDIT_CHECK.json'
+RESULT_SAVE = r'car_release_dates_wiki_final.json'
+
+PLACEHOLDER_RELEASE_DATE = "2000"
+
+data = load_dict_from_json(filename=TARGET_SAVE)
+
+# this section goes through each of the vehicle names, and finds the ones without any valid release date, and then replaces them with a set release date
+replacement_count = 0
+for key, item in data.items():
+    if not contains_valid_year(item['release_date']):
+        print(f'{key} does not have a valid release date, it has {item["release_date"]}')
+        item['release_date'] = PLACEHOLDER_RELEASE_DATE
+        item['source'] = 'replacement'
+        item['snippet'] = 'replacement'
+        replacement_count += 1
+    else:
+        # getting the year itself i guess 
+        year = contains_valid_year(item['release_date'])
+
+
+# then we save the data into a differnet .json file 
+print(f'number of items replaced with placeholder is {replacement_count}')
+save_dict_to_json(data=data, filename=RESULT_SAVE)
+print(f'resulding file is saved to {RESULT_SAVE}')
